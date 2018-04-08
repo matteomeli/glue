63f16da7ea47b71859d7931796d8252e6b3d177c
Matteo Meli <matteo.meli@gmail.com>
2018-04-08 19:18:14 +0100

    Add Show instance for Unit type

--- src/main/scala/glue/typeclass/Show.scala
+++ src/main/scala/glue/typeclass/Show.scala
@@ -47,15 +47,16 @@ trait ShowSyntax {
 }
 
 trait ShowInstances {
+  implicit val unitCanShow: Show[Unit] = Unit => "unit"
   implicit val boolCanShow: Show[Boolean] = bool => s"bool $bool"
   implicit val byteCanShow: Show[Byte] = byte => s"byte $byte"
   implicit val charCanShow: Show[Char] = char => s"char $char"
   implicit val shortCanShow: Show[Short] = short => s"short $short"
   implicit val intCanShow: Show[Int] = int => s"int $int"
-  implicit val bigIntCanShow: Show[BigInt] = bigInt => s"big int $bigInt"
   implicit val longCanShow: Show[Long] = long => s"long $long"
   implicit val floatCanShow: Show[Float] = float => s"float $float"
   implicit val doubleCanShow: Show[Double] = double => s"double $double"
+  implicit val bigIntCanShow: Show[BigInt] = bigInt => s"big int $bigInt"
   implicit val bigDecimalCanShow: Show[BigDecimal] = bigDecimal => s"big decimal $bigDecimal"
   implicit val stringCanShow: Show[String] = str => s"string $str"
 }
