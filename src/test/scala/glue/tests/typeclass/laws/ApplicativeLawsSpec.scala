7440c2c3589179aecbac600fc9ea7b8bc9a0ed60
Matteo Meli <matteo.meli@gmail.com>
2018-04-30 22:43:30 +0100

    Aadd functor laws to applicative laws tests

--- src/test/scala/glue/tests/typeclass/laws/ApplicativeLawsSpec.scala
+++ src/test/scala/glue/tests/typeclass/laws/ApplicativeLawsSpec.scala
@@ -15,14 +15,17 @@ trait ApplicativeLawsSpec[F[_]] {
     arbFC: Arbitrary[F[C]],
     arbFAB: Arbitrary[F[A => B]],
     cogenA: Cogen[A],
+    cogenB: Cogen[B],
     cogenC: Cogen[C]): Properties = new Properties("applicative") {
-    property("leftIdentity") = forAll(laws.leftIdentity[A] _)
-    property("rightIdentity") = forAll(laws.rightIdentity[A] _)
-    property("associativity") = forAll(laws.associativity[A, B, C] _)
-    property("homomorphism") = forAll(laws.homomorphism[A, B] _)
-    property("interchange") = forAll(laws.interchange[A, B] _)
-    property("applicativeMap") = forAll(laws.applicativeMap[A, B] _)
-    property("naturality") = forAll(laws.naturality[A, B, C, D] _)
+      // Any Applicative is also a Functor, so Functor laws should be respected by any Applicative
+      include(FunctorLawsSpec[F](laws.applicative.functor).functor[A, B, C])
+      property("leftIdentity") = forAll(laws.leftIdentity[A] _)
+      property("rightIdentity") = forAll(laws.rightIdentity[A] _)
+      property("associativity") = forAll(laws.associativity[A, B, C] _)
+      property("homomorphism") = forAll(laws.homomorphism[A, B] _)
+      property("interchange") = forAll(laws.interchange[A, B] _)
+      property("applicativeMap") = forAll(laws.applicativeMap[A, B] _)
+      property("naturality") = forAll(laws.naturality[A, B, C, D] _)
   }
 }
 
