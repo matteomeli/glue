8f9aac14a2612919bb4f26b21655e2a0f9cde171
Matteo Meli <matteo.meli@gmail.com>
2018-05-07 15:56:14 +0100

    Add Reader data type

---
+++ src/main/scala/glue/data/Reader.scala
@@ -0,0 +1,43 @@
+package glue.data
+
+import glue.typeclass.{Applicative, Functor, Monad}
+
+final case class Reader[R, A](run: R => A) {
+  def map[B](f: A => B): Reader[R, B] = Reader(r => f(run(r)))
+  def flatMap[B](f: A => Reader[R, B]): Reader[R, B] = Reader(r => f(run(r)).run(r))
+}
+
+object Reader extends ReaderFunctions {
+  object implicits extends ReaderImplicits
+}
+
+trait ReaderFunctions {
+  def apply[R, A](f: R => A): Reader[R, A] = Reader(f)
+  def read[R]: Reader[R, R] = Reader(r => r)
+}
+
+trait ReaderImplicits {
+  implicit def readerIsMonad[R]: Monad[({type f[x] = Reader[R, x]})#f] = new Monad[({type f[x] = Reader[R, x]})#f] {
+    val applicative: Applicative[({type f[x] = Reader[R, x]})#f] = Applicative[({type f[x] = Reader[R, x]})#f]
+    def flatMap[A, B](ra: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] = ra flatMap f
+  }
+
+  private implicit def readerIsApplicative[R]: Applicative[({type f[x] = Reader[R, x]})#f] = new Applicative[({type f[x] = Reader[R, x]})#f] {
+    val functor: Functor[({type f[x] = Reader[R, x]})#f] = Functor[({type f[x] = Reader[R, x]})#f]
+    def unit[A](a: => A): Reader[R, A] = Reader(_ => a)
+    def apply[A, B](rf: Reader[R, A => B])(ra: Reader[R, A]): Reader[R, B] = Reader { r =>
+      val a = ra.run(r)
+      val f = rf.run(r)
+      f(a)
+    }
+
+    override def replicateM[A](n: Int, ra: Reader[R, A]): Reader[R, List[A]] = Reader { r =>
+      val a = ra.run(r)
+      List.fill(n)(a)
+    }
+  }
+
+  private implicit def readerIsFunctor[R]: Functor[({type f[x] = Reader[R, x]})#f] = new Functor[({type f[x] = Reader[R, x]})#f] {
+    def map[A, B](ra: Reader[R, A])(f: A => B): Reader[R, B] = ra map f
+  }
+}
