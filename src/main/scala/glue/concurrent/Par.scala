74d3063abec60157c75ec53ec8a0b4adefcb8b69
Matteo Meli <matteo.meli@gmail.com>
2018-10-08 16:57:41 +0100

    Switch to use lower level java concurrent ExecutorService (infer ctx)

--- src/main/scala/glue/concurrent/Par.scala
+++ src/main/scala/glue/concurrent/Par.scala
@@ -4,20 +4,26 @@ package concurrent
 import scala.concurrent.{ExecutionContext, Future, Await}
 import scala.concurrent.duration.Duration
 
+import java.util.concurrent.ExecutorService
+
 object Par {
-  type Par[A] = ExecutionContext => Future[A]
+  type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: A): Par[A] = (ec: ExecutionContext) => Future.successful(a)
+  def unit[A](a: A): Par[A] = _ => Future.successful(a)
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (ec: ExecutionContext) => {
-    a(ec).zipWith(b(ec)) { (a, b) => f(a, b) }(ec)
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
+    implicit val ctx = ExecutionContext.fromExecutorService(es)
+    a(es).zipWith(b(es)) { (a, b) => f(a, b) }
   }
 
-  def map[A, B](a: Par[A])(f: A => B): Par[B] = map2(a, unit(())) { case (a, _) => f(a) }
+  def map[A, B](a: Par[A])(f: A => B): Par[B] = es => {
+    implicit val ctx = ExecutionContext.fromExecutorService(es)
+    a(es) map f
+  }
 
   // TODO: This doesn't really fork computation 'a' in a separate logical thread,
   // as soon as 'ec' is passed a executes the future into the main thread
-  def fork[A](a: => Par[A]): Par[A] = (ec: ExecutionContext) => a(ec)
+  def fork[A](a: => Par[A]): Par[A] = es => a(es)
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
@@ -48,10 +54,10 @@ object Par {
     map(sequence(pars))(_.flatten)
   }
 
-  def flatMap[A, B](p: Par[A])(f: A => Par[B]): Par[B] =
-    (ec: ExecutionContext) => {
-      val a = run(p)(ec)
-      f(a)(ec)
+  def flatMap[A, B](pa: Par[A])(f: A => Par[B]): Par[B] =
+    es => {
+      implicit val ctx = ExecutionContext.fromExecutorService(es)
+      pa(es) flatMap { a => f(a)(es) }
     }
 
   def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
@@ -65,13 +71,14 @@ object Par {
 
   def join[A](a: Par[Par[A]]): Par[A] = flatMap(a)(identity)
 
-  def equal[A](a1: Par[A], a2: Par[A])(implicit ec: ExecutionContext): Boolean =
-    Await.result(map2(a1, a2)(_ == _)(ec), Duration.Inf)
+  def equal[A](a1: Par[A], a2: Par[A])(implicit es: ExecutorService): Boolean =
+    Await.result(map2(a1, a2)(_ == _)(es), Duration.Inf)
 
-  def run[A](a: Par[A])(implicit ec: ExecutionContext): A = {
+  def run[A](a: Par[A])(implicit es: ExecutorService): A = {
+    implicit val ctx = ExecutionContext.fromExecutorService(es)
     val latch = new java.util.concurrent.CountDownLatch(1)
     @volatile var result: Option[A] = None
-    a(ec) foreach { a => result = Some(a); latch.countDown }
+    a(es) foreach { a => result = Some(a); latch.countDown }
     latch.await
     result.get
   }
@@ -86,7 +93,7 @@ trait ParSyntax {
     def map[B](f: A => B): Par[B] = Par.map(self)(f)
     def map2[B, C](other: Par[B])(f: (A, B) => C): Par[C] = Par.map2(self, other)(f)
     def flatMap[B](f: A => Par[B]): Par[B] = Par.flatMap(self)(f)
-    def equal(other: Par[A])(implicit ec: ExecutionContext): Boolean = Par.equal(self, other)(ec)
-    def run(implicit ec: ExecutionContext): A = Par.run(self)(ec)
+    def equal(other: Par[A])(implicit es: ExecutorService): Boolean = Par.equal(self, other)(es)
+    def run(implicit es: ExecutorService): A = Par.run(self)(es)
   }
 }
