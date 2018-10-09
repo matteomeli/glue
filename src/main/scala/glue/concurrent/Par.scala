90bae98f0c8a1a48c0f9ca27254cc96d2235b89a
Matteo Meli <matteo.meli@gmail.com>
2018-10-09 12:58:57 +0100

    Switch to ADT representation for Par to implement fork correctly

--- src/main/scala/glue/concurrent/Par.scala
+++ src/main/scala/glue/concurrent/Par.scala
@@ -1,47 +1,64 @@
 package glue
 package concurrent
 
-import scala.concurrent.{ExecutionContext, Future, Await}
-import scala.concurrent.duration.Duration
-
 import java.util.concurrent.ExecutorService
 
+sealed trait Par[A] {
+  def map[B](f: A => B): Par[B] = Par.map(this)(f)
+  def flatMap[B](f: A => Par[B]): Par[B] = Par.flatMap(this)(f)
+  def runAsync(cb: A => Unit): Unit = Par.runAsync(this)(cb)
+  def run: A = Par.run(this)
+}
+case class Now[A](value: A) extends Par[A]
+case class Delay[A](t: () => Par[A]) extends Par[A]
+case class Async[A](k: (A => Unit) => Unit) extends Par[A]
+case class Chain[A, B](pa: Par[A], f: A => Par[B]) extends Par[B]
+
 object Par {
-  type Par[A] = ExecutorService => Future[A]
-
-  def unit[A](a: A): Par[A] = _ => Future.successful(a)
-
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
-    implicit val ctx = ExecutionContext.fromExecutorService(es)
-    a(es).zipWith(b(es)) { (a, b) => f(a, b) }
+  def apply[A](a: => A)(implicit es: ExecutorService): Par[A] = Async { cb =>
+    es.execute(new Runnable {
+      def run = cb(a)
+    })
   }
 
-  def map[A, B](a: Par[A])(f: A => B): Par[B] = es => {
-    implicit val ctx = ExecutionContext.fromExecutorService(es)
-    a(es) map f
-  }
+  def now[A](a: A): Par[A] = Now(a)
 
-  // TODO: This doesn't really fork computation 'a' in a separate logical thread,
-  // as soon as 'ec' is passed a executes the future into the main thread
-  def fork[A](a: => Par[A]): Par[A] = es => a(es)
+  def delayNow[A](a: => A): Par[A] = delay(now(a))
 
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def lazyNow[A](a: => A)(implicit es: ExecutorService): Par[A] = fork(now(a))
 
-  def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+  def async[A](k: (A => Unit) => Unit): Par[A] = Async(k)
 
-  def parMap[A, B](as: List[A])(f: A => B): Par[List[B]] = fork {
-    sequence(as map asyncF(f))
-  }
+  def delay[A](pa: => Par[A]): Par[A] = Delay(() => pa)
 
-  def sequence[A](as: List[Par[A]]): Par[List[A]] = as.foldRight(unit(List[A]())) { map2(_, _) { _ :: _ } }
+  def asyncF[A, B](f: A => B)(implicit es: ExecutorService): A => Par[B] = a => lazyNow(f(a))
 
-  def sequenceR[A](as: List[Par[A]]): Par[List[A]] = as match {
-    case Nil => unit(Nil)
+  def fork[A](pa: => Par[A])(implicit es: ExecutorService): Par[A] = join(Par(pa))
+
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] = flatMap(pa)(f andThen (now(_)))
+
+  def flatMap[A, B](pa: Par[A])(f: A => Par[B]): Par[B] = Chain(pa, f)
+
+  def join[A](pa: Par[Par[A]]): Par[A] = flatMap(pa)(identity)
+
+  // TODO
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+/*
+    es => {
+      implicit val ctx = ExecutionContext.fromExecutorService(es)
+      a(es).zipWith(b(es)) { (a, b) => f(a, b) }
+    }
+*/
+
+  def sequence[A](as: List[Par[A]]): Par[List[A]] = as.foldRight(now(List[A]())) { map2(_, _) { _ :: _ } }
+
+  def sequenceR[A](as: List[Par[A]])(implicit es: ExecutorService): Par[List[A]] = as match {
+    case Nil => now(Nil)
     case h :: t => map2(h, fork(sequenceR(t)))(_ :: _)
   }
 
-  def sequenceB[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
-    if (as.isEmpty) unit(Vector()) 
+  def sequenceB[A](as: IndexedSeq[Par[A]])(implicit es: ExecutorService): Par[IndexedSeq[A]] = fork {
+    if (as.isEmpty) now(Vector()) 
     else if (as.length == 1) map(as.head)(Vector(_))
     else {
       val (l, r) = as.splitAt(as.length / 2)
@@ -49,16 +66,14 @@ object Par {
     }
   }
 
-  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = fork {
+  def parMap[A, B](as: List[A])(f: A => B)(implicit es: ExecutorService): Par[List[B]] = fork {
+    sequence(as map asyncF(f))
+  }
+
+  def parFilter[A](as: List[A])(f: A => Boolean)(implicit es: ExecutorService): Par[List[A]] = fork {
     val pars: List[Par[Option[A]]] = as map (asyncF(a => if (f(a)) Some(a) else None))
     map(sequence(pars))(_.flatten)
   }
-
-  def flatMap[A, B](pa: Par[A])(f: A => Par[B]): Par[B] =
-    es => {
-      implicit val ctx = ExecutionContext.fromExecutorService(es)
-      pa(es) flatMap { a => f(a)(es) }
-    }
 
   def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
     flatMap(cond) { if (_) t else f }
@@ -69,31 +84,44 @@ object Par {
   def choiceMap[K, V](key: Par[K])(choices: Map[K, Par[V]]): Par[V] =
     flatMap(key) { choices(_) }
 
-  def join[A](a: Par[Par[A]]): Par[A] = flatMap(a)(identity)
+  def equal[A](a1: Par[A], a2: Par[A]): Boolean = run(a1) == run(a2)
 
-  def equal[A](a1: Par[A], a2: Par[A])(implicit es: ExecutorService): Boolean =
-    Await.result(map2(a1, a2)(_ == _)(es), Duration.Inf)
+  @annotation.tailrec
+  final def step[A](pa: Par[A]): Par[A] = pa match {
+    case Delay(t) => step(t())
+    case Chain(Now(a), f) => step(f(a))
+    case Chain(Delay(t), f) => step(t() flatMap f)
+    case Chain(Chain(x, f), g) => step(x flatMap (f andThen g))
+    case _ => pa
+  }
 
-  def run[A](a: Par[A])(implicit es: ExecutorService): A = {
-    implicit val ctx = ExecutionContext.fromExecutorService(es)
-    val latch = new java.util.concurrent.CountDownLatch(1)
-    @volatile var result: Option[A] = None
-    a(es) foreach { a => result = Some(a); latch.countDown }
-    latch.await
-    result.get
+  def runAsync[A](pa: Par[A])(cb: A => Unit): Unit = step(pa) match {
+    case Now(a) => cb(a)
+    case Async(k) => k(cb)
+    case Chain(Async(k), f) => k { a => runAsync(f(a))(cb) }
+    case _ => sys.error("Impossible since `step` eliminate these cases.")
+  }
+
+  def run[A](pa: Par[A]): A = pa match {
+    case Now(a) => a
+    case _ => {
+      val latch = new java.util.concurrent.CountDownLatch(1)
+      @volatile var result: Option[A] = None
+      runAsync(pa) { a => result = Some(a); latch.countDown }
+      latch.await
+      result.get
+    }
   }
 
   object syntax extends ParSyntax
 }
 
 trait ParSyntax {
-  import Par.Par
-
   implicit class ParOps[A](self: Par[A]) {
     def map[B](f: A => B): Par[B] = Par.map(self)(f)
     def map2[B, C](other: Par[B])(f: (A, B) => C): Par[C] = Par.map2(self, other)(f)
     def flatMap[B](f: A => Par[B]): Par[B] = Par.flatMap(self)(f)
-    def equal(other: Par[A])(implicit es: ExecutorService): Boolean = Par.equal(self, other)(es)
-    def run(implicit es: ExecutorService): A = Par.run(self)(es)
+    def equal(other: Par[A]): Boolean = Par.equal(self, other)
+    def run: A = Par.run(self)
   }
 }
